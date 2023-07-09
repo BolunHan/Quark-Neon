0ee7bb854bf03fdfb8487697a057a5c30e4bc6ad
Bolun <Bolun.Han@outlook.com>
2023-07-09 17:05:42 +0800

    disable timezone awareness by default

--- Quark/Base/_statics.py
+++ Quark/Base/_statics.py
@@ -87,7 +87,7 @@ class _GlobalStatics(object):
         self.CURRENCY = 'CNY'
         self.SHARE = 'SHARE'
         self.TOPIC = _TopicSet
-        self.TIME_ZONE = pytz.timezone('Asia/Shanghai')
+        self.TIME_ZONE = None  # set to pytz.timezone('Asia/Shanghai') to enable timezone awareness
 
         if 'QUARK_CWD' in os.environ:
             self.WORKING_DIRECTORY = _Constants(os.path.realpath(os.environ['QUARK_CWD']))
