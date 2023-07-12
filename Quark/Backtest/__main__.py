7e5dcf5b556ae55ac4881a0603b803a7144b16d2
Bolun <Bolun.Han@outlook.com>
2023-07-12 21:14:57 +0800

    update backtest scripts

--- Quark/Backtest/__main__.py
+++ Quark/Backtest/__main__.py
@@ -1,30 +1,38 @@
 __package__ = 'Quark.Backtest'
 
 import datetime
-import os.path
+import os
 import pathlib
+import sys
 import time
 import uuid
 
+sys.path.append(str(pathlib.Path(__file__).parent.parent.parent))
+
 from . import LOGGER
+from . import factor_pool
 from . import simulated_env
 from ..API import historical
 from ..Base import GlobalStatics
 from ..Calibration.linear import LinearCore
 from ..Misc import helper
-from ..Strategy.data_core import SyntheticIndexMonitor, VolatilityMonitor
-from ..Strategy.decision_core import DummyDecisionCore
-from ..Strategy.strategy import Strategy, StrategyStatus
+from ..Strategy.data_core import SyntheticIndexMonitor, VolatilityMonitor, MDS
+from ..Strategy.strategy import Strategy
 
+# params
 INDEX_NAME = '000016.SH'
 MARKET_DATE = START_DATE = datetime.date(2023, 1, 1)
 END_DATE = datetime.date(2023, 6, 1)
+OVERRIDE_FACTOR_CACHE = False
 TEST_ID = str(uuid.uuid4())
+
+# status
 TEST_ID_SHORT = TEST_ID.split('-')[0]
 IS_INITIALIZED = False
 EPOCH_TS = 0.
 CALENDAR = simulated_env.trade_calendar(start_date=START_DATE, end_date=END_DATE)
-
+FACTORS = ['Monitor.TradeFlow.EMA', 'Monitor.Coherence.Price.EMA', 'Monitor.Coherence.Volume', 'Monitor.SyntheticIndex', 'Monitor.TA.MACD', 'Monitor.Aggressiveness.EMA', 'Monitor.Entropy.Price.EMA']
+FACTOR_POOL = factor_pool.FACTOR_POOL
 STRATEGY = Strategy(
     index_ticker=INDEX_NAME,
     index_weights=helper.load_dict(
@@ -82,6 +90,16 @@ def bod(market_date: datetime.date, **kwargs):
     historical.unzip_batch(market_date=market_date, ticker_list=subscription)
     STRATEGY.subscription.clear()
 
+    # backtest specific action 2: load factor pool
+    FACTOR_POOL.load(market_date=MARKET_DATE)
+    factor_existed = FACTOR_POOL.monitor_names(market_date=MARKET_DATE)
+    if factor_existed and not OVERRIDE_FACTOR_CACHE:
+        LOGGER.info(f'FACTOR_POOL loaded factors {factor_existed}, using local caches!')
+        factor_tasks = [_ for _ in FACTORS if _ not in factor_existed]
+
+    else:
+        factor_tasks = FACTORS
+
     if 'replay' in kwargs:
         replay = kwargs['replay']
 
@@ -97,11 +115,13 @@ def bod(market_date: datetime.date, **kwargs):
 
     # startup task 2: update monitors
     # for backtest purpose, the monitors is only registered once
-    if not IS_INITIALIZED:
-        monitors = STRATEGY.register()
-    else:
-        monitors = STRATEGY.monitors
-        STRATEGY.status = StrategyStatus.working
+    STRATEGY.clear()
+    monitors = STRATEGY.register(factors=factor_tasks)
+
+    if not OVERRIDE_FACTOR_CACHE:
+        factor_dummy_monitor = factor_pool.FactorPoolDummyMonitor()
+        MDS.add_monitor(factor_dummy_monitor)
+        STRATEGY.monitors[factor_dummy_monitor.name] = factor_dummy_monitor
 
     # OPTIONAL: task 2.1: update baseline for Monitor.SyntheticIndex
     monitor: SyntheticIndexMonitor = monitors.get('Monitor.SyntheticIndex')
@@ -144,12 +164,12 @@ def bod(market_date: datetime.date, **kwargs):
         monitor.synthetic_base_price = simulated_env.query_daily(ticker=INDEX_NAME, market_date=MARKET_DATE, key='preclose')
 
     # startup task 3: initialize decision core
+    STRATEGY.decision_core = LinearCore(ticker=INDEX_NAME, decode_level=3, data_source=dump_dir)
     try:
-        STRATEGY.decision_core = LinearCore(ticker=INDEX_NAME, decode_level=3, data_source=dump_dir)
         STRATEGY.decision_core.load(file_dir=dump_dir, file_pattern=r'decision_core\.(\d{4}-\d{2}-\d{2})\.json')
     except Exception as _:
-        STRATEGY.decision_core = DummyDecisionCore()  # in production mode, just throw the error and stop the program
-        LOGGER.warning('decision core not loaded, using dummy core!')
+        # STRATEGY.decision_core = DummyDecisionCore()  # in production mode, just throw the error and stop the program
+        LOGGER.warning(f'{market_date} failed to load decision core!')
 
     # backtest-specific codes
     if not IS_INITIALIZED:
@@ -166,12 +186,23 @@ def eod(market_date: datetime.date = MARKET_DATE, **kwargs):
 
     os.makedirs(dump_dir, exist_ok=True)
 
-    STRATEGY.position_tracker.clear()
-    STRATEGY.strategy_metric.dump(dump_dir.joinpath(f'metric.{MARKET_DATE}.csv'))
-    STRATEGY.decision_core.calibrate(metric=STRATEGY.strategy_metric)
+    # EoD task 0: update FACTOR_POOL and serialize factors
+    FACTOR_POOL.batch_update(factors=STRATEGY.strategy_metric.factor_value, exclude_keys=FACTOR_POOL.factor_names(market_date=MARKET_DATE))
+    FACTOR_POOL.dump()
+
+    # EoD task 1: calibrate decision core
+    cal_report = STRATEGY.decision_core.calibrate(metric=STRATEGY.strategy_metric, market_date=MARKET_DATE)  # in production mode, the market_date in kwargs is optional
+    if cal_report:
+        LOGGER.info(f'calibration report:\n' + '\n'.join([f"{_}: {cal_report[_]}" for _ in cal_report]))
     STRATEGY.decision_core.dump(dump_dir.joinpath(f'decision_core.{MARKET_DATE}.json'))
-    STRATEGY.strategy_metric.clear()
-    STRATEGY.decision_core.clear()
+
+    # OPTIONAL EoD task 2: dump metrics, to validate factor cache
+    STRATEGY.strategy_metric.dump(dump_dir.joinpath(f'metric.{MARKET_DATE}.csv'))
+
+    # OPTIONAL EoD task 3: clear and reset environment
+    STRATEGY.clear()
+    FACTOR_POOL.clear()
+
     LOGGER.info(f'Backtest epoch {market_date} complete! Time costs {time.time() - EPOCH_TS}')
 
 
