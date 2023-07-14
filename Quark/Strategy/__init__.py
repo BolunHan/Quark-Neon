cc9f1c9e011a26c5606801f24ade1013dc78f851
Bolun <Bolun.Han@outlook.com>
2023-07-14 14:29:25 +0800

    add __all__

--- Quark/Strategy/__init__.py
+++ Quark/Strategy/__init__.py
@@ -3,6 +3,27 @@ from AlgoEngine.Strategies import STRATEGY_ENGINE
 
 from .. import LOGGER
 
-__all__ = ['LOGGER', 'STRATEGY_ENGINE', 'MDS']
-
 LOGGER = LOGGER.getChild('Strategy')
+
+from .data_core import *
+from .decision_core import *
+from .decoder import *
+from .metric import *
+from .strategy import *
+
+__all__ = [
+    # Basic
+    'LOGGER', 'STRATEGY_ENGINE', 'MDS',
+    # names from .data_core
+    'IndexWeight', 'Synthetic', 'EMA', 'TradeFlowMonitor', 'TradeFlowEMAMonitor', 'CoherenceMonitor', 'CoherenceEMAMonitor', 'TradeCoherenceMonitor',
+    'SyntheticIndexMonitor', 'MACDMonitor', 'AggressivenessMonitor', 'AggressivenessEMAMonitor', 'EntropyMonitor', 'EntropyEMAMonitor',
+    'VolatilityMonitor', 'DecoderMonitor', 'IndexDecoderMonitor', 'register_monitor',
+    # names from .decision_core
+    'DecisionCore', 'DummyDecisionCore',
+    # names from .decoder
+    'WaveletFlag', 'Wavelet', 'Decoder', 'OnlineDecoder', 'RecursiveDecoder',
+    # names from .metric
+    'StrategyMetric',
+    # names from .strategy
+    'StrategyStatus', 'Strategy',
+]
