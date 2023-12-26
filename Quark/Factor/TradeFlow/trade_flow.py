4ac29e1aa6c1867354cfa219c94ce103d591d0d6
bolun <Bolun.Han@outlook.com>
2023-12-27 01:13:20 +0800

    refactor the return types of .value and add docstring

--- Quark/Factor/TradeFlow/trade_flow.py
+++ Quark/Factor/TradeFlow/trade_flow.py
@@ -1,3 +1,21 @@
+"""
+This module defines two classes, TradeFlowMonitor and TradeFlowEMAMonitor, for monitoring the net trade volume flow of a given underlying.
+
+Classes:
+- TradeFlowMonitor: Monitors net trade volume flow for each underlying and provides the values.
+- TradeFlowEMAMonitor: Extends TradeFlowMonitor and adds Exponential Moving Averages (EMA) to trade flow and trade volume.
+
+Usage:
+1. Instantiate the desired monitor class with optional parameters.
+2. Call the instance with market data to update the monitor.
+3. Retrieve the calculated values using the 'value' property.
+
+Note: This module assumes the availability of AlgoEngine, PyQuantKit, and other required modules.
+
+Author: Bolun
+Date: 2023-12-27
+"""
+
 from AlgoEngine.Engine import MarketDataMonitor
 from PyQuantKit import MarketData, TradeData
 
@@ -6,7 +24,11 @@ from .. import EMA, Synthetic, MDS
 
 class TradeFlowMonitor(MarketDataMonitor):
     """
-    monitor net trade volume flow of given underlying, with unit of share
+    Monitors net trade volume flow for each underlying and provides the values, with unit of share.
+
+    Args:
+    - name (str, optional): Name of the monitor. Defaults to 'Monitor.TradeFlow'.
+    - monitor_id (str, optional): Identifier for the monitor. Defaults to None.
     """
 
     def __init__(self, name: str = 'Monitor.TradeFlow', monitor_id: str = None):
@@ -16,10 +38,22 @@ class TradeFlowMonitor(MarketDataMonitor):
         self._is_ready = True
 
     def __call__(self, market_data: MarketData, **kwargs):
+        """
+        Update the monitor based on market data.
+
+        Args:
+        - market_data (MarketData): Market data to update the monitor.
+        """
         if isinstance(market_data, TradeData):
             return self._on_trade(trade_data=market_data)
 
     def _on_trade(self, trade_data: TradeData):
+        """
+        Handle trade data to update net trade volume flow.
+
+        Args:
+        - trade_data (TradeData): Trade data to handle.
+        """
         ticker = trade_data.ticker
         volume = trade_data.volume
         side = trade_data.side.sign
@@ -27,24 +61,41 @@ class TradeFlowMonitor(MarketDataMonitor):
         self._trade_flow[ticker] = self._trade_flow.get(ticker, 0.) + volume * side
 
     def clear(self):
+        """Clear the monitor data."""
         self._trade_flow.clear()
 
     @property
     def value(self) -> dict[str, float]:
+        """
+        Get the net trade volume flow values.
+
+        Returns:
+        dict[str, float]: Dictionary of net trade volume flow values.
+        """
         return self._trade_flow
 
     @property
     def is_ready(self) -> bool:
+        """
+        Check if the monitor is ready.
+
+        Returns:
+        bool: True if the monitor is ready, False otherwise.
+        """
         return self._is_ready
 
 
 class TradeFlowEMAMonitor(TradeFlowMonitor, EMA, Synthetic):
     """
-    ema discounted trade flow
-    Note that the discount process is triggered by on_trade_data. Upon discount, there should be a discontinuity of index value.
-    But if the update interval is small enough, the effect should be marginal
+    Exponential Moving Average (EMA) of net trade volume flow.
 
-    trade flow is a relative stable indication. a large positive aggressiveness indicates market is in an upward ongoing trend
+    Args:
+    - discount_interval (float): Interval for EMA discounting.
+    - alpha (float): EMA smoothing factor.
+    - weights (dict[str, float]): Dictionary of ticker weights.
+    - normalized (bool, optional): Whether to use normalized EMA. Defaults to True.
+    - name (str, optional): Name of the monitor. Defaults to 'Monitor.TradeFlow.EMA'.
+    - monitor_id (str, optional): Identifier for the monitor. Defaults to None.
     """
 
     def __init__(self, discount_interval: float, alpha: float, weights: dict[str, float], normalized: bool = True, name: str = 'Monitor.TradeFlow.EMA', monitor_id: str = None):
@@ -58,6 +109,12 @@ class TradeFlowEMAMonitor(TradeFlowMonitor, EMA, Synthetic):
         self._trade_volume = self._register_ema(name='trade_volume')
 
     def _on_trade(self, trade_data: TradeData):
+        """
+        Handle trade data to update net trade volume flow and accumulate EMAs.
+
+        Args:
+        - trade_data (TradeData): Trade data to handle.
+        """
         ticker = trade_data.ticker
         volume = trade_data.volume
         side = trade_data.side.sign
@@ -66,6 +123,12 @@ class TradeFlowEMAMonitor(TradeFlowMonitor, EMA, Synthetic):
         self._accumulate_ema(ticker=ticker, timestamp=timestamp, trade_flow=volume * side, trade_volume=volume)
 
     def __call__(self, market_data: MarketData, **kwargs):
+        """
+        Update the monitor and trigger EMA discounting based on market data.
+
+        Args:
+        - market_data (MarketData): Market data to update the monitor.
+        """
         ticker = market_data.ticker
         timestamp = market_data.timestamp
 
@@ -75,12 +138,18 @@ class TradeFlowEMAMonitor(TradeFlowMonitor, EMA, Synthetic):
         super().__call__(market_data=market_data, **kwargs)
 
     def clear(self):
+        """Clear the monitor data."""
         super().clear()
         EMA.clear(self)
         Synthetic.clear(self)
 
-    @property
-    def value(self) -> dict[str, float]:
+    def trade_flow_adjusted(self):
+        """
+        Get adjusted net trade volume flow values.
+
+        Returns:
+        dict[str, float]: Dictionary of adjusted net trade volume flow values.
+        """
         if self.normalized:
             normalized_trade_flow = {}
 
@@ -92,12 +161,31 @@ class TradeFlowEMAMonitor(TradeFlowMonitor, EMA, Synthetic):
                 normalized_trade_flow[ticker] = adjusted_flow
 
                 if abs(adjusted_flow) > 1:
-                    raise ValueError('adjusted_flow should not larger than 1 or smaller than -1, check the code!')
+                    raise ValueError('adjusted_flow should not be larger than 1 or smaller than -1, check the code!')
 
             return normalized_trade_flow
         else:
             return super().value
 
     @property
+    def value(self) -> dict[str, float]:
+        """
+        Get the adjusted values of net trade volume flow and the composite index value.
+
+        Returns:
+        dict[str, float]: Dictionary of adjusted values.
+        """
+        result = {}
+        result.update(self.trade_flow_adjusted())
+        result['Index'] = self.index_value
+        return result
+
+    @property
     def index_value(self) -> float:
+        """
+        Get the composite index value.
+
+        Returns:
+        float: Composite index value.
+        """
         return self.composite(self.value)
