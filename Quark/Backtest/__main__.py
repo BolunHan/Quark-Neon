b9b11a097f51951512c242e05670caafc98a1054
Bolun <Bolun.Han@outlook.com>
2024-01-28 18:37:03 +0800

    add more caching functions

--- Quark/Backtest/__main__.py
+++ Quark/Backtest/__main__.py
@@ -3,13 +3,15 @@ __package__ = 'Quark.Backtest'
 import datetime
 import os
 import pathlib
+import pickle
+import random
 import sys
 import time
 import traceback
 
 import pandas as pd
 from AlgoEngine.Engine import ProgressiveReplay, SimMatch
-from PyQuantKit import TradeData, TransactionSide, MarketData
+from PyQuantKit import MarketData, TickData
 
 sys.path.append(str(pathlib.Path(__file__).parent.parent.parent))
 
@@ -17,12 +19,12 @@ from . import LOGGER
 from . import simulated_env
 from ..API import historical, external
 from ..Base import GlobalStatics
-from ..DecisionCore.decision_core import MajorityDecisionCore as Core
 from ..DataLore.data_lore import LinearDataLore
+from ..DecisionCore import DummyDecisionCore, MajorityDecisionCore as Core
 from ..Factor import *
 from ..Factor.factor_pool import FACTOR_POOL, FactorPoolDummyMonitor
-from ..Strategy import Strategy, MDS
 from ..Profile import cn
+from ..Strategy import Strategy, MDS
 
 DUMMY_WEIGHT = False
 cn.profile_cn_override()
@@ -37,7 +39,7 @@ class BackTest(object):
         self.override_cache = kwargs.get('override_cache', False)
         self.use_dummy_core = kwargs.get('dummy_core', False)
         self.sampling_interval = kwargs.get('sampling_interval', 10.)
-        self.cache_dir = kwargs.get('cache_dir', pathlib.Path(GlobalStatics.WORKING_DIRECTORY.value, 'Res', 'Factors'))
+        self.res_dir = kwargs.get('res_dir', pathlib.Path(GlobalStatics.WORKING_DIRECTORY.value, 'Res'))
         self.test_id = kwargs.get('test_id', self._get_test_id())
         self.dump_dir = kwargs.get('dump_dir', pathlib.Path(GlobalStatics.WORKING_DIRECTORY.value, f'TestResult.{self.test_id}'))
         self.calendar = simulated_env.trade_calendar(start_date=self.start_date, end_date=self.end_date)
@@ -77,6 +79,7 @@ class BackTest(object):
         self.event_engine = self.engine.event_engine
         self.topic_set = self.engine.topic_set
         self.sim_match = {}
+        self.fake_market_data: list[TickData] = []
 
     def _preload_cache(self, look_back: int = 90):
         start_date = self.start_date - datetime.timedelta(days=look_back)
@@ -123,6 +126,9 @@ class BackTest(object):
         self.strategy.subscription.clear()
         self.strategy.subscription.update(subscription)
 
+        # add additional subscription for strategy: the index tick data (not available for replay api)
+        self.strategy.subscription.add(self.index_name)
+
     @classmethod
     def _get_test_id(cls):
         test_id = 1
@@ -148,35 +154,39 @@ class BackTest(object):
         strategy.engine.add_handler(on_eod=self.eod)
         return strategy
 
-    def _sim_match_synthetic(self, timestamp: float, index_price: float):
+    def _sim_match_synthetic(self, market_data: MarketData, sampling_interval: float = 1.):
+        timestamp = market_data.timestamp
+
         if self.index_name in self.sim_match:
             sim_match = self.sim_match[self.index_name]
         else:
             sim_match = self.sim_match[self.index_name] = SimMatch(ticker=self.index_name)
             sim_match.register(event_engine=self.event_engine, topic_set=self.topic_set)
 
-        # simulate 2 index trade data
-        fake_data = TradeData(
-            ticker=self.index_name,
-            side=TransactionSide.LongOpen,
-            trade_volume=1,
-            timestamp=timestamp,
-            trade_price=index_price
-        )
+        # the matching is done at external process
+        if market_data.ticker == self.index_name:
+            pass
+        # avoid over-sampling to preserve resources
+        elif self.fake_market_data and int(self.fake_market_data[-1].timestamp // sampling_interval) == int(timestamp // sampling_interval):
+            pass
+        # simulate the index data for match making and cache
+        else:
+            index_price = self.synthetic.index_price
 
-        MDS.on_market_data(market_data=fake_data)
-        sim_match.__call__(market_data=fake_data)
+            # simulate 2 index tick data
+            fake_data = TickData(
+                ticker=self.index_name,
+                timestamp=timestamp,
+                last_price=index_price
+            )
 
-        fake_data = TradeData(
-            ticker=self.index_name,
-            side=TransactionSide.ShortOpen,
-            trade_volume=1,
-            timestamp=timestamp,
-            trade_price=index_price
-        )
+            MDS.on_market_data(market_data=fake_data)
 
-        MDS.on_market_data(market_data=fake_data)
-        sim_match.__call__(market_data=fake_data)
+            self.engine.mds.on_market_data(market_data=fake_data)
+            self.engine.position_tracker.on_market_data(market_data=fake_data)
+            self.engine.__call__(market_data=fake_data)
+            sim_match.__call__(market_data=fake_data)
+            self.fake_market_data.append(fake_data)
 
     def initialize_factor(self) -> list[FactorMonitor]:
         self.factor = [
@@ -247,19 +257,42 @@ class BackTest(object):
 
         return self.factor
 
-    def initialize_cache(self, market_date: datetime.date):
+    def initialize_cache(self, market_date: datetime.date, replay: ProgressiveReplay):
         if self.override_cache:
             return
 
-        self.factor_pool.load(market_date=market_date, factor_dir=self.cache_dir)
+        # load fake data
+        fake_data_path = self.res_dir.joinpath('Data', f'{self.index_name}.{market_date:%Y-%m-%d}.fake_data.pkl')
+        if os.path.isfile(fake_data_path):
+            LOGGER.info(f'Loading {market_date} cached {self.index_name} index data from {fake_data_path}')
+            with open(fake_data_path, 'rb') as f:
+                self.fake_market_data.extend(pickle.load(f))
+
+        # load factor cache
+        self.factor_pool.load(market_date=market_date, factor_dir=self.res_dir.joinpath('Factors'))
         factor_existed = self.factor_pool.factor_names(market_date=market_date)
 
+        # mask cached monitors
         for factor in self.factor:
             factor_names = factor.factor_names(subscription=list(self.strategy.subscription))
 
             if all([_ in factor_existed for _ in factor_names]):
                 factor.enabled = False
                 LOGGER.info(f'Factor {factor.name} found in the factor cache, and will be disabled.')
+            else:
+                LOGGER.warning(f'Factor {factor.name} cache not hit! Recommend to use validation script first before back test.')
+
+        # add fake data to replay
+        if self.fake_market_data:
+            if all([not factor.enabled for factor in self.factor]):
+                replay.replay_subscription.clear()
+                self.strategy.subscription.clear()  # need to ensure the synchronization of the subscription
+                self.strategy.subscription.add(self.index_name)
+                LOGGER.info(f'{market_date} All factor is cached, components replay tasks skipped.')
+
+            # replay.add_subscription(ticker=self.index_name, dtype=TickData)
+            replay.replay_task.extend(self.fake_market_data)
+            LOGGER.info(f'{self.index_name} data cache found, added to the replay tasks.')
 
     def initialize_baseline(self, market_date: datetime.date):
         for monitor in self.factor + [self.synthetic]:
@@ -302,13 +335,19 @@ class BackTest(object):
         data_lore.inputs_var.extend(self.features)
 
         try:
-            self.strategy.decision_core = self.strategy.decision_core.load(
-                file_dir=self.dump_dir,
-                file_pattern=r'decision_core\.(\d{4}-\d{2}-\d{2})\.json'
-            )
+            if os.path.isdir(self.dump_dir):
+                self.strategy.decision_core = self.strategy.decision_core.load(
+                    file_dir=self.dump_dir,
+                    file_pattern=r'decision_core\.(\d{4}-\d{2}-\d{2})\.json'
+                )
+            else:
+                LOGGER.error(f'{market_date} decision core can not be loaded, invalid directory {self.dump_dir}.')
+        except FileNotFoundError as _:
+            file_path = self.dump_dir.joinpath(r'decision_core\.(\d{4}-\d{2}-\d{2})\.json')
+            LOGGER.error(f'{market_date} decision core can not be loaded, invalid file path {file_path}.')
         except Exception as _:
-            # STRATEGY.decision_core = DummyDecisionCore()  # in production mode, just throw the error and stop the program
-            LOGGER.warning(f'{market_date} failed to load decision core! traceback: {traceback.format_exc()}')
+            self.strategy.decision_core = DummyDecisionCore()  # in production mode, just throw the error and stop the program
+            LOGGER.error(f'{market_date} failed to load decision core! Fallback to dummy core!\ntraceback:\n{traceback.format_exc()}')
 
     def initialize_position_management(self):
         from AlgoEngine.Strategies import RISK_PROFILE
@@ -319,19 +358,26 @@ class BackTest(object):
         # RISK_PROFILE.set_rule(ticker=self.index_name, key='max_exposure_short', value=100)
 
     def update_cache(self, market_date: datetime):
+        # dump fake market data
+        fake_data_path = self.res_dir.joinpath('Data', f'{self.index_name}.{market_date:%Y-%m-%d}.fake_data.pkl')
+        os.makedirs(fake_data_path.parent, exist_ok=True)
+        with open(fake_data_path, 'wb') as f:
+            pickle.dump(self.fake_market_data, f)
+
+        # dump factor pool
         if self.override_cache:
             LOGGER.info(f'Cache {market_date} overridden!')
             self.factor_pool.batch_update(factors=self.strategy.strategy_metric.factor_value)
+            self.factor_pool.dump(factor_dir=self.res_dir.joinpath('Factors'))
         else:
             exclude_keys = self.factor_pool.factor_names(market_date=market_date)
             self.factor_pool.batch_update(factors=self.strategy.strategy_metric.factor_value, exclude_keys=exclude_keys)
 
             if all([name in exclude_keys for name in pd.DataFrame(self.strategy.strategy_metric.factor_value).T.columns]):
-                return
-
-            LOGGER.info('Cache updated!')
-
-        self.factor_pool.dump(factor_dir=self.cache_dir)
+                pass
+            else:
+                self.factor_pool.dump(factor_dir=self.res_dir.joinpath('Factors'))
+                LOGGER.info('Cache updated!')
 
     def update_decision_core(self, market_date: datetime.date):
         # in production mode, the market_date in kwargs is optional
@@ -342,7 +388,8 @@ class BackTest(object):
         )
 
         self.strategy.decision_core.validation(
-            factor_value=self.strategy.strategy_metric.factor_value
+            factor_value=self.strategy.strategy_metric.factor_value,
+            candle_sticks=self.strategy.strategy_metric.candle_sticks
         )
 
     def dump_result(self, market_date: datetime.date):
@@ -356,18 +403,21 @@ class BackTest(object):
         self.strategy.strategy_metric.dump(dump_dir.joinpath(f'metrics.{market_date}.csv'))
 
         # dump trade summaries
-        self.strategy.strategy_metric.trade_info.to_csv(dump_dir.joinpath(f'trade.summary.{market_date}.csv'))
+        self.strategy.strategy_metric.trade_summary.to_csv(dump_dir.joinpath(f'trade.summary.{market_date}.csv'))
 
         # dump backtest prediction
         self.strategy.strategy_metric.plot_prediction().write_html(dump_dir.joinpath(f'metrics.prediction.{market_date}.html'))
 
         # dump backtest trades
         self.strategy.strategy_metric.plot_trades().write_html(dump_dir.joinpath(f'metrics.trades.{market_date}.html'))
+        self.strategy.strategy_metric.signal_trade_metrics.info.to_csv(dump_dir.joinpath(f'trade.signal.{market_date}.csv'))
+        self.strategy.strategy_metric.target_trade_metrics.info.to_csv(dump_dir.joinpath(f'trade.target.{market_date}.csv'))
+        self.strategy.strategy_metric.actual_trade_metrics.info.to_csv(dump_dir.joinpath(f'trade.actual.{market_date}.csv'))
 
         # dump in-sample validations
         for pred_var, cv in self.strategy.decision_core.data_lore.cv.items():
             fig = cv.plot()
-            fig.write_html(dump_dir.joinpath(f'{pred_var}.in_sample.html'))
+            fig.write_html(dump_dir.joinpath(f'val.{pred_var}.in_sample.html'))
 
     def reset(self):
         self.factor.clear()
@@ -375,6 +425,8 @@ class BackTest(object):
         self.strategy.clear()
         self.factor_pool.clear()
         self.factor_cache.clear()
+        self.sim_match.clear()
+        self.fake_market_data.clear()
 
     def run(self, **kwargs):
         self._preload_cache()
@@ -412,7 +464,8 @@ class BackTest(object):
                 self.engine.position_tracker.on_market_data(market_data=market_data)
                 self.engine.__call__(market_data=market_data)
                 self.sim_match[ticker](market_data=market_data)
-            self._sim_match_synthetic(timestamp=market_data.timestamp, index_price=self.synthetic.index_price)
+
+            self._sim_match_synthetic(market_data=market_data)
 
         LOGGER.info(f'All done! time_cost: {time.time() - start_ts:,.3}s')
 
@@ -436,7 +489,7 @@ class BackTest(object):
         self.initialize_factor()
 
         # Backtest specific Startup task 3.1: Update caches
-        self.initialize_cache(market_date=market_date)
+        self.initialize_cache(market_date=market_date, replay=replay)
 
         # startup task 4: initialize baseline
         self.initialize_baseline(market_date=market_date)
@@ -456,6 +509,9 @@ class BackTest(object):
     def eod(self, market_date: datetime.date, **kwargs) -> None:
         if market_date not in self.calendar:
             return
+
+        # EoD task -1: randomness cancellation patch
+        random.seed(42)
 
         # EoD task 0: dump factor cache
         self.update_cache(market_date=market_date)
@@ -478,7 +534,7 @@ def main():
     tester = BackTest(
         index_name='000016.SH',
         start_date=datetime.date(2023, 1, 1),
-        end_date=datetime.date(2023, 6, 1)
+        end_date=datetime.date(2023, 2, 1)
     )
 
     tester.run()
