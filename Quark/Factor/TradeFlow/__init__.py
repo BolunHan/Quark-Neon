5d8feff1f770b9893a6b6d9770c7faea6bc61e47
Bolun <Bolun.Han@outlook.com>
2024-01-30 16:16:51 +0800

    updated trade flow factor

--- Quark/Factor/TradeFlow/__init__.py
+++ Quark/Factor/TradeFlow/__init__.py
@@ -1,13 +1,14 @@
 from .aggressiveness import AggressivenessMonitor, AggressivenessEMAMonitor
-from .trade_flow import TradeFlowMonitor, TradeFlowEMAMonitor
-from .. import add_monitor, ALPHA_05, ALPHA_0001, INDEX_WEIGHTS
+from .trade_flow import TradeFlowMonitor, TradeFlowAdaptiveMonitor, TradeFlowAdaptiveIndexMonitor
+from .. import add_monitor, ALPHA_0001, INDEX_WEIGHTS
 
 MONITOR = {}
 
-add_monitor(TradeFlowMonitor(), monitors=MONITOR, register=False)
-add_monitor(TradeFlowEMAMonitor(discount_interval=1, alpha=ALPHA_05, weights=INDEX_WEIGHTS), monitors=MONITOR, register=False)
+add_monitor(TradeFlowMonitor(sampling_interval=15, sample_size=20), monitors=MONITOR, register=False)
+add_monitor(TradeFlowAdaptiveIndexMonitor(sampling_interval=15, sample_size=20, baseline_window=100, weights=INDEX_WEIGHTS), monitors=MONITOR, register=False)
 
 add_monitor(AggressivenessMonitor(), monitors=MONITOR, register=False)
 add_monitor(AggressivenessEMAMonitor(discount_interval=1, alpha=ALPHA_0001, weights=INDEX_WEIGHTS), monitors=MONITOR, register=False)
 
-__all__ = ['AggressivenessMonitor', 'AggressivenessEMAMonitor', 'TradeFlowMonitor', 'TradeFlowEMAMonitor']
+__all__ = ['AggressivenessMonitor', 'AggressivenessEMAMonitor',
+           'TradeFlowMonitor', 'TradeFlowAdaptiveMonitor', 'TradeFlowAdaptiveIndexMonitor']
--- Quark/Factor/TradeFlow/trade_flow.py
+++ Quark/Factor/TradeFlow/trade_flow.py
@@ -1,82 +1,227 @@
-"""
-This module defines two classes, TradeFlowMonitor and TradeFlowEMAMonitor, for monitoring the net trade volume flow of a given underlying.
+from collections import deque
+from typing import Iterable
 
-Classes:
-- TradeFlowMonitor: Monitors net trade volume flow for each underlying and provides the values.
-- TradeFlowEMAMonitor: Extends TradeFlowMonitor and adds Exponential Moving Averages (EMA) to trade flow and trade volume.
-
-Usage:
-1. Instantiate the desired monitor class with optional parameters.
-2. Call the instance with market data to update the monitor.
-3. Retrieve the calculated values using the 'value' property.
-
-Note: This module assumes the availability of AlgoEngine, PyQuantKit, and other required modules.
-
-Author: Bolun
-Date: 2023-12-27
-"""
-
+import numpy as np
 from PyQuantKit import MarketData, TradeData, TransactionData
 
-from .. import EMA, Synthetic, FactorMonitor
+from .. import Synthetic, FactorMonitor, FixedIntervalSampler, AdaptiveVolumeIntervalSampler
 
 
-class TradeFlowMonitor(FactorMonitor):
-    """
-    Monitors net trade volume flow for each underlying and provides the values, with unit of share.
+class TradeFlowMonitor(FactorMonitor, FixedIntervalSampler):
 
-    Args:
-    - name (str, optional): Name of the monitor. Defaults to 'Monitor.TradeFlow'.
-    - monitor_id (str, optional): Identifier for the monitor. Defaults to None.
-    """
-
-    def __init__(self, name: str = 'Monitor.TradeFlow', monitor_id: str = None):
+    def __init__(self, sampling_interval: float, sample_size: int, name: str = 'Monitor.TradeFlow', monitor_id: str = None):
         super().__init__(name=name, monitor_id=monitor_id)
+        FixedIntervalSampler.__init__(self=self, sampling_interval=sampling_interval, sample_size=sample_size)
 
-        self._trade_flow = dict()
+        self.register_sampler(name='price', mode='update')
+        self.register_sampler(name='trade_flow', mode='accumulate')
+        self.register_sampler(name='volume', mode='accumulate')
+
+        self._historical_trade_imbalance = {}
         self._is_ready = True
 
-    def __call__(self, market_data: MarketData, **kwargs):
-        """
-        Update the monitor based on market data.
-
-        Args:
-        - market_data (MarketData): Market data to update the monitor.
-        """
-        if isinstance(market_data, (TradeData, TransactionData)):
-            return self._on_trade(trade_data=market_data)
-
-    def _on_trade(self, trade_data: TradeData | TransactionData):
-        """
-        Handle trade data to update net trade volume flow.
-
-        Args:
-        - trade_data: Trade data to handle.
-        """
+    def on_trade_data(self, trade_data: TradeData | TransactionData, **kwargs):
         ticker = trade_data.ticker
+        timestamp = trade_data.timestamp
+        market_price = trade_data.market_price
         volume = trade_data.volume
         side = trade_data.side.sign
 
-        self._trade_flow[ticker] = self._trade_flow.get(ticker, 0.) + volume * side
+        self.log_obs(ticker=ticker, timestamp=timestamp, price=market_price, volume=volume, trade_flow=volume * side)
+
+    def on_entry_added(self, ticker: str, name: str, value):
+        super().on_entry_added(ticker=ticker, name=name, value=value)
+
+        if name != 'trade_flow':
+            return
+
+        trade_imbalance_dict = self.trade_imbalance(ticker=ticker, drop_last=True, boosted=False)
+
+        if ticker not in trade_imbalance_dict:
+            return
+
+        trade_imbalance = trade_imbalance_dict[ticker]
+
+        if not np.isfinite(trade_imbalance):
+            return
+
+        if ticker in self._historical_trade_imbalance:
+            historical_trade_imbalance = self._historical_trade_imbalance[ticker]
+        else:
+            historical_trade_imbalance = self._historical_trade_imbalance[ticker] = deque(maxlen=max(5, int(self.sample_size / 2)))
+
+        historical_trade_imbalance.append(trade_imbalance)
+
+    def slope(self) -> dict[str, float]:
+        slope_dict = {}
+        for ticker in self._historical_trade_imbalance:
+            trade_imbalance = list(self._historical_trade_imbalance[ticker])
+
+            if len(trade_imbalance) < 3:
+                slope = np.nan
+            else:
+                x = list(range(len(trade_imbalance)))
+                x = np.vstack([x, np.ones(len(x))]).T
+                y = np.array(trade_imbalance)
+
+                slope, c = np.linalg.lstsq(x, y, rcond=None)[0]
+
+            slope_dict[ticker] = slope
+
+        return slope_dict
 
     def clear(self):
-        """Clear the monitor data."""
-        self._trade_flow.clear()
+        self._historical_trade_imbalance.clear()
+        FixedIntervalSampler.clear(self)
 
     def factor_names(self, subscription: list[str]) -> list[str]:
         return [
             f'{self.name.removeprefix("Monitor.")}.{ticker}' for ticker in subscription
         ]
 
-    @property
-    def value(self) -> dict[str, float]:
+    @classmethod
+    def sampling(cls, observation: np.ndarray, n_bins: int):
         """
-        Get the net trade volume flow values.
+        Sample continuous variables X1 and X2 into discrete bins based on quantiles.
+
+        Parameters:
+        - observation: 2D array representing observations of X1 and X2.
+        - n_bins: Number of bins for discretization.
 
         Returns:
-        dict[str, float]: Dictionary of net trade volume flow values.
+        - joint_distribution: 2D array representing the joint distribution of variables X1 and X2.
         """
-        return self._trade_flow
+        # Discretize X1 and X2 into bins
+        x1_bins = np.linspace(np.min(observation[:, 0]), np.max(observation[:, 0]), n_bins + 1)
+        x2_bins = np.linspace(np.min(observation[:, 1]), np.max(observation[:, 1]), n_bins + 1)
+
+        # Compute indices of the bins for each observation
+        x1_indices = np.digitize(observation[:, 0], x1_bins)
+        x2_indices = np.digitize(observation[:, 1], x2_bins)
+
+        # Count observations falling into each bin
+        joint_distribution, _, _ = np.histogram2d(x1_indices, x2_indices, bins=(range(1, len(x1_bins) + 1), range(1, len(x2_bins) + 1)))
+
+        # Normalize joint distribution
+        joint_distribution /= np.sum(joint_distribution)
+
+        return joint_distribution
+
+    @classmethod
+    def mutual_information(cls, joint_distribution: np.ndarray, epsilon: float = 1e-10):
+        """
+        Compute the mutual information of two variables given their joint distribution.
+
+        Parameters:
+        - joint_distribution: 2D array representing the joint distribution of variables X1 and X2.
+
+        Returns:
+        - Mutual information (float).
+        """
+        # Compute marginal distributions
+        marginal_x1 = np.sum(joint_distribution, axis=1)
+        marginal_x2 = np.sum(joint_distribution, axis=0)
+
+        # Compute individual entropies
+        entropy_x1 = -np.sum(marginal_x1 * np.log(marginal_x1 + epsilon))  # Adding a small epsilon to avoid log(0)
+        entropy_x2 = -np.sum(marginal_x2 * np.log(marginal_x2 + epsilon))
+        entropy_joint = -np.sum(joint_distribution * np.log(joint_distribution + epsilon))
+
+        # Compute mutual information
+        mutual_information = entropy_x1 + entropy_x2 - entropy_joint
+
+        return mutual_information
+
+    def trade_imbalance_info(self):
+        """
+        I(trade_imbalance; price_pct) = H(x1) + H(x2) - H(x1, x2)
+        Returns:
+        """
+        volume_dict = self.get_sampler(name='volume')
+        trade_flow_dict = self.get_sampler(name='trade_flow')
+        price_dict = self.get_sampler(name='price')
+        result = {}
+
+        for ticker in volume_dict:
+            price_vector = np.array(list(price_dict[ticker].values()))
+            price_pct_vector = np.diff(price_vector) / price_vector[:-1]
+            volume_vector = np.array(list(volume_dict[ticker].values()))[1:]
+            trade_flow_vector = np.array(list(trade_flow_dict[ticker].values()))[1:]
+            trade_imbalance = np.array([trade_flow / volume if volume else 0. for trade_flow, volume in zip(trade_flow_vector, volume_vector)])
+            observation = np.array([price_pct_vector, trade_imbalance]).T
+
+            u_u = np.sum([1 for price_pct, trade_imbalance in zip(price_pct_vector, trade_imbalance) if price_pct > 0 and trade_imbalance > 0])
+            u_d = np.sum([1 for price_pct, trade_imbalance in zip(price_pct_vector, trade_imbalance) if price_pct > 0 and trade_imbalance < 0])
+            d_u = np.sum([1 for price_pct, trade_imbalance in zip(price_pct_vector, trade_imbalance) if price_pct < 0 and trade_imbalance > 0])
+            d_d = np.sum([1 for price_pct, trade_imbalance in zip(price_pct_vector, trade_imbalance) if price_pct < 0 and trade_imbalance < 0])
+            n = u_u + u_d + d_u + d_d
+
+            if not n:
+                continue
+
+            joint_distribution = np.array([
+                [u_u / n, u_d / n],
+                [d_u / n, d_d / n]
+            ])
+
+            # joint_distribution = self.sampling(observation=observation, n_bins=4)
+            # mutual_information = self.mutual_information(joint_distribution=joint_distribution)
+            joint_info = -np.sum(joint_distribution * np.log(joint_distribution + 1e-5))
+
+            result[ticker] = joint_info
+
+        return result
+
+    def trade_imbalance(self, ticker: str = None, drop_last: bool = False, boosted: bool = True):
+        volume_dict = self.get_sampler(name='volume')
+        trade_flow_dict = self.get_sampler(name='trade_flow')
+        price_dict = self.get_sampler(name='price')
+        result = {}
+
+        if ticker is None:
+            tasks = list(volume_dict)
+        elif isinstance(ticker, str):
+            tasks = [ticker]
+        elif isinstance(ticker, Iterable):
+            tasks = list(ticker)
+        else:
+            raise TypeError(f'Invalid ticker {ticker}, expect str, list[str] or None.')
+
+        for ticker in tasks:
+            price_vector = list(price_dict[ticker].values())
+            volume_vector = list(volume_dict[ticker].values())[1:]
+            trade_flow_vector = list(trade_flow_dict[ticker].values())[1:]
+
+            if len(price_vector) < 4:
+                continue
+
+            if drop_last:
+                price_vector.pop(-1)
+                volume_vector.pop(-1)
+                trade_flow_vector.pop(-1)
+
+            price_pct_vector = np.diff(price_vector) / np.array(price_vector)[:-1]
+            trade_imbalance = np.array([trade_flow / volume if volume else 0. for trade_flow, volume in zip(trade_flow_vector, volume_vector)])
+
+            if boosted:
+                u_u = np.sum([trade_imbalance * price_pct for price_pct, trade_imbalance in zip(price_pct_vector, trade_imbalance) if price_pct > 0 and trade_imbalance > 0])
+                d_d = np.sum([trade_imbalance * price_pct for price_pct, trade_imbalance in zip(price_pct_vector, trade_imbalance) if price_pct < 0 and trade_imbalance < 0])
+
+                result[ticker] = u_u - d_d
+            else:
+                u_u = np.sum([trade_imbalance for price_pct, trade_imbalance in zip(price_pct_vector, trade_imbalance) if price_pct > 0 and trade_imbalance > 0])
+                u_d = np.sum([trade_imbalance for price_pct, trade_imbalance in zip(price_pct_vector, trade_imbalance) if price_pct > 0 and trade_imbalance < 0])
+                d_u = np.sum([trade_imbalance for price_pct, trade_imbalance in zip(price_pct_vector, trade_imbalance) if price_pct < 0 and trade_imbalance > 0])
+                d_d = np.sum([trade_imbalance for price_pct, trade_imbalance in zip(price_pct_vector, trade_imbalance) if price_pct < 0 and trade_imbalance < 0])
+
+                result[ticker] = u_u + d_d + u_d + d_u
+                result[ticker] = u_u + d_d
+
+        return result
+
+    @property
+    def value(self) -> dict[str, float]:
+        return self.trade_imbalance(boosted=True)
 
     @property
     def is_ready(self) -> bool:
@@ -89,117 +234,85 @@ class TradeFlowMonitor(FactorMonitor):
         return self._is_ready
 
 
-class TradeFlowEMAMonitor(TradeFlowMonitor, EMA, Synthetic):
-    """
-    Exponential Moving Average (EMA) of net trade volume flow.
+class TradeFlowAdaptiveMonitor(TradeFlowMonitor, AdaptiveVolumeIntervalSampler):
 
-    Args:
-    - discount_interval (float): Interval for EMA discounting.
-    - alpha (float): EMA smoothing factor.
-    - weights (dict[str, float]): Dictionary of ticker weights.
-    - normalized (bool, optional): Whether to use normalized EMA. Defaults to True.
-    - name (str, optional): Name of the monitor. Defaults to 'Monitor.TradeFlow.EMA'.
-    - monitor_id (str, optional): Identifier for the monitor. Defaults to None.
-    """
+    def __init__(self, sampling_interval: float, sample_size: int = 20, baseline_window: int = 100, aligned_interval: bool = False, name: str = 'Monitor.TradeFlow.Adaptive', monitor_id: str = None):
+        super().__init__(
+            sampling_interval=sampling_interval,
+            sample_size=sample_size,
+            name=name,
+            monitor_id=monitor_id
+        )
 
-    def __init__(self, discount_interval: float, alpha: float, weights: dict[str, float], normalized: bool = True, name: str = 'Monitor.TradeFlow.EMA', monitor_id: str = None):
-        super().__init__(name=name, monitor_id=monitor_id)
-        EMA.__init__(self=self, discount_interval=discount_interval, alpha=alpha)
-        Synthetic.__init__(self=self, weights=weights)
-
-        self.normalized = normalized
-
-        self._trade_flow = self.register_ema(name='trade_flow')
-        self._trade_volume = self.register_ema(name='trade_volume')
-
-    def _on_trade(self, trade_data: TradeData | TransactionData):
-        """
-        Handle trade data to update net trade volume flow and accumulate EMAs.
-
-        Args:
-        - trade_data: Trade data to handle.
-        """
-        ticker = trade_data.ticker
-        volume = trade_data.volume
-        side = trade_data.side.sign
-        timestamp = trade_data.timestamp
-
-        self.accumulate_ema(ticker=ticker, timestamp=timestamp, trade_flow=volume * side, trade_volume=volume)
+        AdaptiveVolumeIntervalSampler.__init__(
+            self=self,
+            sampling_interval=sampling_interval,
+            sample_size=sample_size,
+            baseline_window=baseline_window,
+            aligned_interval=aligned_interval
+        )
 
     def __call__(self, market_data: MarketData, **kwargs):
-        """
-        Update the monitor and trigger EMA discounting based on market data.
+        self.accumulate_volume(market_data=market_data)
+        super().__call__(market_data=market_data, **kwargs)
 
-        Args:
-        - market_data (MarketData): Market data to update the monitor.
-        """
+    def clear(self) -> None:
+        super().clear()
+        AdaptiveVolumeIntervalSampler.clear(self)
+
+    @property
+    def is_ready(self) -> bool:
+        for ticker in self._volume_baseline['obs_vol_acc']:
+            if ticker not in self._volume_baseline['sampling_interval']:
+                return False
+
+        return self._is_ready
+
+
+class TradeFlowAdaptiveIndexMonitor(TradeFlowAdaptiveMonitor, Synthetic):
+
+    def __init__(self, sampling_interval: float, sample_size: int, baseline_window: int, aligned_interval: bool = False, weights: dict[str, float] = None, name: str = 'Monitor.TradeFlow.Adaptive.Index', monitor_id: str = None):
+        super().__init__(
+            sampling_interval=sampling_interval,
+            sample_size=sample_size,
+            baseline_window=baseline_window,
+            aligned_interval=aligned_interval,
+            name=name,
+            monitor_id=monitor_id
+        )
+
+        Synthetic.__init__(self=self, weights=weights)
+
+    def __call__(self, market_data: MarketData, **kwargs):
         ticker = market_data.ticker
-        timestamp = market_data.timestamp
 
-        self.discount_ema(ticker=ticker, timestamp=timestamp)
-        self.discount_all(timestamp=timestamp)
+        if self.weights and ticker not in self.weights:
+            return
 
         super().__call__(market_data=market_data, **kwargs)
 
-    def clear(self):
-        """Clear the monitor data."""
+    def clear(self) -> None:
         super().clear()
-        EMA.clear(self)
         Synthetic.clear(self)
-
-        self._trade_flow = self.register_ema(name='trade_flow')
-        self._trade_volume = self.register_ema(name='trade_volume')
-
-    def trade_flow_adjusted(self):
-        """
-        Get adjusted net trade volume flow values.
-
-        Returns:
-        dict[str, float]: Dictionary of adjusted net trade volume flow values.
-        """
-        if self.normalized:
-            normalized_trade_flow = {}
-
-            for ticker in self._trade_flow:
-                net_flow = self._trade_flow[ticker]
-                volume = self._trade_volume[ticker]
-                adjusted_flow = net_flow / volume if volume else 0.
-
-                normalized_trade_flow[ticker] = adjusted_flow
-
-                if abs(adjusted_flow) > 1:
-                    raise ValueError('adjusted_flow should not be larger than 1 or smaller than -1, check the code!')
-
-            return normalized_trade_flow
-        else:
-            return super().value
 
     def factor_names(self, subscription: list[str]) -> list[str]:
         return [
-            f'{self.name.removeprefix("Monitor.")}.Index'
-        ] + [
-            f'{self.name.removeprefix("Monitor.")}.{ticker}' for ticker in subscription
+            f'{self.name.removeprefix("Monitor.")}.Imbalance',
+            f'{self.name.removeprefix("Monitor.")}.Entropy',
+            f'{self.name.removeprefix("Monitor.")}.Boosted',
+            f'{self.name.removeprefix("Monitor.")}.Slope',
         ]
 
     @property
     def value(self) -> dict[str, float]:
-        """
-        Get the adjusted values of net trade volume flow and the composite index value.
+        trade_imbalance = self.trade_imbalance(boosted=False)
+        trade_imbalance_info = self.trade_imbalance_info()
+        trade_imbalance_boosted = self.trade_imbalance(boosted=True)
+        slope = self.slope()
 
-        Returns:
-        dict[str, float]: Dictionary of adjusted values.
-        """
-        result = {}
-        result.update(self.trade_flow_adjusted())
-        result['Index'] = self.composite(result)
-        return result
-
-    @property
-    def index_value(self) -> float:
-        """
-        Get the composite index value.
-
-        Returns:
-        float: Composite index value.
-        """
-        return self.composite(self.trade_flow_adjusted())
+        return {
+            'Imbalance': self.composite(values=trade_imbalance),
+            'Entropy': self.composite(values=trade_imbalance_info),
+            'Boosted': self.composite(values=trade_imbalance_boosted),
+            'Slope': self.composite(values=slope)
+        }
