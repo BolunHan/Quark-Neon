0c76218fbf83674250ccd70280bb19b69d5de93d
Bolun <Bolun.Han@outlook.com>
2024-03-06 11:57:09 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+import setuptools
+import os
+import codecs
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            # __version__ = "0.9"
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+long_description = read("README.md")
+
+setuptools.setup(
+    name="Quark",
+    version=get_version(os.path.join('Quark', '__init__.py')),
+    author="Bolun.Han",
+    author_email="Bolun.Han@outlook.com",
+    description="HFT Factor Mining Platform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BolunHan/Quark",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+    install_requires=[
+        'PyAlgoEngine',
+        'PyQuantKit',
+        'pandas',
+        'numpy',
+        'scipy',
+    ]
+)
